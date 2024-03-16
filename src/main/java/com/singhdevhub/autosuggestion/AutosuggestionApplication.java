7db4af6bafabe2394e8fc64c926fe2bbe53fe70a
singhdevhub-lovepreet <lpskshorts@gmail.com>
2024-03-16 15:01:14 +0530

    initial template of the project

---
+++ src/main/java/com/singhdevhub/autosuggestion/AutosuggestionApplication.java
@@ -0,0 +1,13 @@
+package com.singhdevhub.autosuggestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosuggestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosuggestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singhdevhub/autosuggestion/AutosuggestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singhdevhub.autosuggestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosuggestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

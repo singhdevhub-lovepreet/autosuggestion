b19ab95d5f74a43d47b8f360a85d86bf8aa8789a
singhdevhub-lovepreet <lpskshorts@gmail.com>
2024-04-06 18:18:05 +0530

    Fixes around mongo redis and configs

--- src/main/java/com/singhdevhub/autosuggestion/AutosuggestionApplication.java
+++ src/main/java/com/singhdevhub/autosuggestion/AutosuggestionApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value = {"com.singhdevhub.autosuggestion/service/", "com.singhdevhub.autosuggestion/config/"})
+@ComponentScan(value = {"com.singhdevhub.autosuggestion/service/", "com.singhdevhub.autosuggestion/config/",
+"com.singhdevhub.autosuggestion/handler/"})
 public class AutosuggestionApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/singhdevhub/autosuggestion/handler/WebSocketConfig.java
+++ src/main/java/com/singhdevhub/autosuggestion/handler/WebSocketConfig.java
@@ -2,19 +2,21 @@ package com.singhdevhub.autosuggestion.handler;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@Controller
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
 {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // Register a WebSocket endpoint
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/hello").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
